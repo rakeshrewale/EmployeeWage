02a78e9ecff6a768c5f46c54e3ef515fc388a938
rakeshrewale <rakeshrewale993@gmail.com>
2022-05-25 22:57:38 +0530

    Added: attendanceLogic

--- src/www/EmployeeWage/concept/EmployeeWageComp.java
+++ src/www/EmployeeWage/concept/EmployeeWageComp.java
@@ -5,6 +5,14 @@ public class EmployeeWageComp {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to EmployeeWageComputation ");
+		
+		int isFullTime=1;
+		
+		double empCheck=Math.floor(Math.random() * 10) % 2;
+		if (empCheck == isFullTime)
+			System.out.println("Employee is present");
+		else
+			System.out.println("Employee is absent");
 
 	}
 
