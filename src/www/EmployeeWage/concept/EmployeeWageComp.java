1cdf62d5e77a5ef7243683eaeb30c427082dd746
rakeshrewale <rakeshrewale993@gmail.com>
2022-05-25 22:50:29 +0530

    Added: gitignore and README file

---
+++ src/www/EmployeeWage/concept/EmployeeWageComp.java
@@ -0,0 +1,11 @@
+package www.EmployeeWage.concept;
+
+public class EmployeeWageComp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to EmployeeWageComputation ");
+
+	}
+
+}
