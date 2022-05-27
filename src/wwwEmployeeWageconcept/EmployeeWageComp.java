08d013508c3e6a8a20fe9e4997a3a9046b23a915
rakeshrewale <rakeshrewale993@gmail.com>
2022-05-27 14:57:14 +0530

    Added: Emp part time wage logic

--- src/wwwEmployeeWageconcept/EmployeeWageComp.java
+++ src/wwwEmployeeWageconcept/EmployeeWageComp.java
@@ -9,20 +9,32 @@ public class EmployeeWageComp {
 		System.out.println("Welcome to EmployeeWageComputation ");
 		Random random = new Random();
 		
-		int attendance = random.nextInt(2);
-		//if (attendance == 1)
-		//int wagePerHour = 20;
-		//int fullDayHours = 8;
-		//int wagePerDay = 0;
+		//int attendance = random.nextInt(2);
+		int attendance = random.nextInt(3);
 		
-		if (attendance == 1) 
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int wagePerDay = 0;
+		int partTimeHours=4;
+		
+		if (attendance == 1)
+		{
 				System.out.println("Employee is present");
 	
-				//wagePerDay = fullDayHours * wagePerHour;
-				//System.out.println("Employee's daily wage is:" + wagePerDay);
-		else	
+				wagePerDay = fullDayHours * wagePerHour;
+				System.out.println("Employee's daily wage is:" + wagePerDay);
+		}
+		else if(attendance ==2)
+		{
+			System.out.println("Employee is present for part time");
+			
+			wagePerDay = partTimeHours * wagePerHour;
+			System.out.println("Employee's daily wage is:" + wagePerDay);
+		}
+		else
 				System.out.println("Employee is absent");
 	}
+		
 		
 }
 
