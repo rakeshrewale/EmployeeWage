a1bb9b8e2871050f2b7103edeaad3218227b5222
rakeshrewale <rakeshrewale993@gmail.com>
2022-05-27 12:58:33 +0530

    Added: Wase-Per-Day

---
+++ src/wwwEmployeeWageconcept/EmployeeWageComp.java
@@ -0,0 +1,29 @@
+package wwwEmployeeWageconcept;
+
+import java.util.Random;
+
+public class EmployeeWageComp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to EmployeeWageComputation ");
+		Random random = new Random();
+		
+		int attendance = random.nextInt(2);
+		//if (attendance == 1)
+		int wagePerHour = 20;
+		int fullDayHours = 8;
+		int wagePerDay = 0;
+		
+		if (attendance == 1) {
+				System.out.println("Employee is present");
+	
+				wagePerDay = fullDayHours * wagePerHour;
+				System.out.println("Employee's daily wage is:" + wagePerDay);
+		}else	
+				System.out.println("Employee is absent");
+	}
+		
+}
+
+
